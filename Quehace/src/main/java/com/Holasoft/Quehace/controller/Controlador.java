0178a75a1e551c373c4386ea513c827365758eaf
F. Rafael Alvarez <fraq86@gmail.com>
2023-10-24 21:10:32 -0300

    Algunas diferencias entre Controller y RestController

--- Quehace/src/main/java/com/Holasoft/Quehace/controller/Controlador.java
+++ Quehace/src/main/java/com/Holasoft/Quehace/controller/Controlador.java
@@ -1,11 +1,10 @@
 package com.Holasoft.Quehace.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class Controlador {
 
@@ -14,8 +13,28 @@ public class Controlador {
         return "Hola mundo";
     }
 
-    @GetMapping("/saludoOtraVez") //Ejemplo http://localhost:8080/saludoOtraVez?numerito=3
+    @GetMapping("/saludoOtraVez/{numerito}") //Ejemplo http://localhost:8080/saludoOtraVez?numerito=3
     public String saludarotraVez(@RequestParam int numerito){
         return "Sos un "+numerito;
     }
+
+    //@ResponseBody renderiza el nombre del archivo en lugar de el archivo en si cuando se usa @Controller
+    @GetMapping("/Chief")
+    public String mostrarChief(Model model){
+        model.addAttribute("casco","Maestro");
+
+        return "masterChief.html";
+    }
+
+    /*
+    @RestController
+    public class MyRestController {  ejemplo sacado de stack overflow para renderizar con @RestController
+    @RequestMapping("/")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login.html");
+        return modelAndView;
+    }
+    }
+    */
 }
