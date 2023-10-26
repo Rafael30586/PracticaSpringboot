7ca46f9906c61896ab1fd6011c573888b5d96817
F. Rafael Alvarez <fraq86@gmail.com>
2023-10-26 09:51:34 -0300

    Uso de ModelAndView

--- Quehace/src/main/java/com/Holasoft/Quehace/controller/Controlador.java
+++ Quehace/src/main/java/com/Holasoft/Quehace/controller/Controlador.java
@@ -3,8 +3,9 @@ package com.Holasoft.Quehace.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class Controlador {
 
@@ -14,16 +15,18 @@ public class Controlador {
     }
 
     @GetMapping("/saludoOtraVez/{numerito}") //Ejemplo http://localhost:8080/saludoOtraVez?numerito=3
-    public String saludarotraVez(@RequestParam int numerito){
+    public String saludarotraVez(@PathVariable int numerito){
         return "Sos un "+numerito;
     }
 
     //@ResponseBody renderiza el nombre del archivo en lugar de el archivo en si cuando se usa @Controller
     @GetMapping("/Chief")
-    public String mostrarChief(Model model){
+    public ModelAndView mostrarChief(Model model){
         model.addAttribute("casco","Maestro");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("masterChief.html");
 
-        return "masterChief.html";
+        return modelAndView;
     }
 
     /*
