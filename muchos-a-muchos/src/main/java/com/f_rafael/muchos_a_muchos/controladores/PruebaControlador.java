8766bc73281a136dedb535ed6c292f8ce7ca1b48
F. Rafael Alvarez <fraq86@gmail.com>
2024-08-30 12:04:04 -0300

    Practica con ResponseEntity

--- muchos-a-muchos/src/main/java/com/f_rafael/muchos_a_muchos/controladores/PruebaControlador.java
+++ muchos-a-muchos/src/main/java/com/f_rafael/muchos_a_muchos/controladores/PruebaControlador.java
@@ -1,8 +1,11 @@
 package com.f_rafael.muchos_a_muchos.controladores;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/prueba")
@@ -11,5 +14,16 @@ public class PruebaControlador {
     @GetMapping("/saludo")
     public String saludar(){
         return "Probando...probando...";
+    }
+
+    @GetMapping("/respuesta-exitosa")
+    public ResponseEntity<String> responder(){
+        return ResponseEntity.ok("Respuesta exitosa");
+    }
+
+    @GetMapping("/paises")
+    public ResponseEntity<List<String>> respoder2(){
+        List<String> paises = List.of("Argentina","Hungria","Corea del sur","Bielorusia");
+        return ResponseEntity.ok(paises);
     }
 }
